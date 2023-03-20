f7dbb11916758abf6acae866695cea1b8be9caf4
Minh Tran <tdaimike@gmail.com>
2023-03-19 22:26:32 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(plan = 'roller-skate') {
+    return `This Saturday, I want to ${plan}!`;
+}
+function mondayWork(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective(emphatic) {
+   
+    return function symbol(result) {
+       
+        return `You are ${emphatic}${result}${emphatic}!`;
+        }
+    }
+
+wrapAdjective('*')('a hard worker');
